d5af7c8111337f8d5f04a3b50c8aaa7df6ff3d27
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-11-24 11:08:18 -0200

    SolidSphere class created

---
+++ FCGFileSystem/SolidSphere.hpp
@@ -0,0 +1,14 @@
+//
+//  SolidSphere.hpp
+//  FCGFileSystem
+//
+//  Created by Henrique Valcanaia on 11/24/15.
+//  Copyright © 2015 PietroHenrique. All rights reserved.
+//
+
+#ifndef SolidSphere_hpp
+#define SolidSphere_hpp
+
+#include <stdio.h>
+
+#endif /* SolidSphere_hpp */
