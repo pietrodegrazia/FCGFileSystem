14be7eb934ef8f7c46fb99196716f2b632f88167
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-12-04 14:00:13 -0200

    Paths

--- FCGFileSystem/Render.cpp
+++ FCGFileSystem/Render.cpp
@@ -32,8 +32,6 @@ GLuint      texture;         /* Texture object */
 void initTexture(void){
     printf ("Loading texture..\n");
     // Load a texture object (256x256 true color)
-//    bits = LoadDIBitmap("/Users/valcanaia/Documents/UFRGS/2015-2/FCG/Pratica/Exercise_x3/FPS/res/tiledbronze.bmp", &info);
-//    bits = LoadDIBitmap("/Users/pietrodegrazia/Documents/UFRGS/FCG/FCGFileSystem 2/FCGFileSystem/circuit.bmp", &info);
     bits = LoadDIBitmap("/Users/valcanaia/Documents/UFRGS/2015-2/FCG/Trabalho Final/FCGFileSystem/FCGFileSystem/circuit.bmp", &info);
     if (bits == (GLubyte *)0) {
         printf ("Error loading texture!\n\n");
